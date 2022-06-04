c4cb55dfd5a026b6edf767c8a6e30b9d97a444bc
Varghese K James <53941989+VicFic2006@users.noreply.github.com>
2022-06-04 15:41:02 +0530

    Create about.py

---
+++ cogs/about.py
@@ -0,0 +1,39 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class about(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+    
+    @app_commands.command(name="help", description="shows help")
+    async def help(self, interaction: discord.Interaction):
+        embedVar = discord.Embed(
+        title="I am Minnu",
+        description="I am a simple bot created by VicFic",
+        color=0xC7DDFC)
+#        embedVar.set_image(url = '')
+        embedVar.add_field(name="GENERAL COMMANDS", value="`/help` `/hello` `/ping` `/getpfp`", inline=False)
+        embedVar.add_field(name="YOUTUBE COMMANDS", value=" `/play` `/stop`", inline=False)
+        await interaction.response.send_message(embed=embedVar)
+
+    @app_commands.command(name="hello", description="Hello!")
+    async def hello(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message(f"Hello {interaction.user.name}!")
+
+    @app_commands.command(name="getpfp", description="Get a user's profile picture")
+    async def getpfp(self, interaction: discord.Interaction, user: discord.Member = None) -> None:
+        user = user or interaction.user
+        await interaction.response.send_message(interaction.user.avatar)
+
+    @app_commands.command(name="ping", description="Get the Discord Websocket Protocol Latency")
+    async def ping(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message(f"My ping is {round(self.bot.latency * 1000)}ms")
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(
+        about(bot),
+        ## The server ID's should be added in a list as discord.Object
+        guilds = [discord.Object(id = 'SERVER_ID')]
+    )
